6d583d92fbed2119ff221f568be1eb820716b6ad
Samuel Wise <samuelwise54@gmail.com>
2020-08-20 23:04:11 -0400

    first time

--- GitUseExample/AppDelegate.swift
+++ GitUseExample/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Application did finish launching")
         // Override point for customization after application launch.
         return true
     }
--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+  print("This is a Git tutorial")
+}
 
 
 }

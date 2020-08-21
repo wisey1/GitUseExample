636e20a128c9cdfb8d2db5c9f98ead3baaf7b8b0
Samuel Wise <samuelwise54@gmail.com>
2020-08-20 22:54:19 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by user180288 on 8/20/20.
+//  Copyright © 2020 Sam Wise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
